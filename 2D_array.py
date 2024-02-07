8cacd4cb376f0af907eb8e993d74659112cb6420
Umwe <99398212+Umwe@users.noreply.github.com>
2024-02-07 13:03:56 +0200

    Update 2D_array.py

--- 2D_array.py
+++ 2D_array.py
@@ -1,1 +1,21 @@
 
+print("Chapter 2 Test by Kwizera Africa Hubert Bonaparte")
+months = [
+    ["January", "February", "March"],
+    ["April", "May", "June"],
+    ["July", "August", "September"],
+    ["October", "November", "December"]
+]
+
+
+row = 0
+col = 0
+
+while row < len(months):
+    month = months[row][col]
+    print(month)
+    
+    col += 1
+    if col >= len(months[row]):
+        col = 0
+        row += 1
